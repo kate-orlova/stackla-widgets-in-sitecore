2c4098bb2447780788e52dcc9880fc1e7fca94c1
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-20 12:03:40 +0000

    Implemented loadStacklaWidget()

--- src/Feature/StacklaWidgets/Assets/StacklaWidget.js
+++ src/Feature/StacklaWidgets/Assets/StacklaWidget.js
@@ -6,6 +6,12 @@ class StacklaWidget extends Component {
     }
 
     loadStacklaWidget(d, id) {
+        if (d.getElementById(id)) return;
+        var t = d.createElement('script');
+        t.type = 'text/javascript';
+        t.src = 'https://assetscdn.stackla.com/media/js/widget/fluid-embed.js';
+        t.id = id;
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(t);
     }
 }
 export default StacklaWidget;
