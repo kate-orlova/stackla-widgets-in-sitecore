f643ee16d282c86a44c02dec7252546f90030a6f
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-25 23:27:52 +0000

    Defined a StacklaId property in StacklaWidget model

--- src/Feature/StacklaWidgets/Models/StacklaWidget.cs
+++ src/Feature/StacklaWidgets/Models/StacklaWidget.cs
@@ -1,6 +1,10 @@
-﻿namespace StacklaWidgets.Models
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace StacklaWidgets.Models
 {
     public class StacklaWidget
     {
+        [SitecoreField("Stackla Id")]
+        public virtual string StacklaId { get; set; }
     }
 }
