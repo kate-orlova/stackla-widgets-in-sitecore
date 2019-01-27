ed429312f430b1e0cfcf23c338129234fd785883
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-27 13:01:12 +0000

    Implemented initComponents()

--- src/Feature/StacklaWidgets/Scripts/Index.js
+++ src/Feature/StacklaWidgets/Scripts/Index.js
@@ -7,10 +7,34 @@ class App {
         this.components = config.components || {};
         this.initComponents();
     }
-    initComponents(target = this.mount) {
+
+    initComponents(container = this.mount) {
+        const elementNodes = [
+            ...container.querySelectorAll(`[data-component]`)
+        ];
+
+        elementNodes.forEach((elementNode) => {
+            const element = elementNode;
+
+            const requiredComponents = element.dataset.component.split(' ');
+
+            requiredComponents.forEach((component) => {
+                const Module = this.components[component];
+                if (Module) {
+
+                    const moduleInstance = new Module({
+                        mount: this.mount,
+                        root: element
+                    });
+                    element.initalisedComponents[component] = moduleInstance;
+                    element.components.push(moduleInstance);
+                }
+            });
+
+        });
     }
-  
 }
+
 const app = new App({
     components: {
         loadStacklaWidget: LoadStacklaWidget
